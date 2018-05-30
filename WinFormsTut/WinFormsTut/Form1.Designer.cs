f3b90789a74ced36be805caa249650b0f0a04d79
LIAM\Liam <liamw98@hotmail.com>
2018-05-31 04:33:24 +1000

    Added in Winforms Application

---
+++ WinFormsTut/WinFormsTut/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace WinFormsTut
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.item1ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.item2ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(666, 199);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(109, 66);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Click Me!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.button1_KeyDown);
+            this.button1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.button1_MouseMove);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(556, 226);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Click The Button!";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(583, 284);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(192, 139);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1,
+            this.toolStripMenuItem2});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.MdiWindowListItem = this.toolStripMenuItem2;
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 3;
+            this.menuStrip1.Text = "menuStrip1";
+            this.menuStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.menuStrip1_ItemClicked);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.item1ToolStripMenuItem,
+            this.item2ToolStripMenuItem});
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(37, 20);
+            this.toolStripMenuItem1.Text = "File";
+            this.toolStripMenuItem1.Click += new System.EventHandler(this.toolStripMenuItem1_Click);
+            // 
+            // item1ToolStripMenuItem
+            // 
+            this.item1ToolStripMenuItem.Name = "item1ToolStripMenuItem";
+            this.item1ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.item1ToolStripMenuItem.Text = "New";
+            this.item1ToolStripMenuItem.Click += new System.EventHandler(this.item1ToolStripMenuItem_Click);
+            // 
+            // item2ToolStripMenuItem
+            // 
+            this.item2ToolStripMenuItem.Name = "item2ToolStripMenuItem";
+            this.item2ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.item2ToolStripMenuItem.Text = "Open";
+            this.item2ToolStripMenuItem.Click += new System.EventHandler(this.item2ToolStripMenuItem_Click);
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(63, 20);
+            this.toolStripMenuItem2.Text = "Window";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
+            this.IsMdiContainer = true;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem item1ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem item2ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem2;
+    }
+}
+
---
+++ WinFormsTut/WinFormsTut/Form1.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+//Winforms is a eventbased graphic handler
+//need to set isMDIcontainer so it may hold other windows
+
+namespace WinFormsTut
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ////Create a form
+            //Form secondForm = new Form();
+
+            ////Show the form
+            //secondForm.Show();  ///If we dont show the form, we cant see it
+
+            ///Create the dialog
+            OpenFileDialog dialog = new OpenFileDialog();
+
+            ///Show dialog
+            DialogResult result = dialog.ShowDialog();
+
+            if (result == DialogResult.OK)
+            {
+                //Shows the file in the dialog box
+                MessageBox.Show(dialog.FileName);
+            }
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            ///Casts textbox into texbox
+            TextBox tb = sender as TextBox;
+            if (tb == null)
+            {
+                ///Raise an Error
+                return;
+            }
+
+            if (tb.Text.ToLower().EndsWith("awesome"))
+            {
+                MessageBox.Show("Its Awesome!");
+            }
+        }
+
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+           //MessageBox.Show(e.ClickedItem.Text);
+        }
+
+        private void button1_KeyDown(object sender, KeyEventArgs e)
+        {
+            MessageBox.Show(e.KeyData.ToString());
+        }
+
+        private void button1_MouseMove(object sender, MouseEventArgs e)
+        {
+            Console.WriteLine(e.X + " " + e.Y);
+        }
+
+        private void item1ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //Create new window
+            TextWindow newTextWindow = new TextWindow();
+
+            //Set MDI parent
+            //Tells the window which parent it is apart of
+            newTextWindow.MdiParent = this;
+
+            //Show the Window
+            newTextWindow.Show();
+            
+        }
+
+        private void item2ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //create an open file dialog
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            //Set the dialog to be navigated to the users My Documents
+            //folder by default.
+            string myDocumentsPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments); //Finds us the users My Documents folder
+            openFileDialog.InitialDirectory = myDocumentsPath;
+
+            //Open the dialog, store the result when done
+            DialogResult result = openFileDialog.ShowDialog();
+
+            //if the dialog was used successfully, attempt to open the
+            //selected file.
+            if (result == DialogResult.OK)
+            {
+                try
+                {
+                    //Get the text
+                    string text = System.IO.File.ReadAllText(openFileDialog.FileName); //We attempt to read the file and return to a string
+
+                    //Create a new text form
+                    TextWindow textForm = new TextWindow();
+
+                    //Insert the read text into the form
+                    textForm.MainTextArea.Text = text;  //Grab the main text area contect
+
+                    //Set the forms parent
+                    textForm.MdiParent = this;          //Sets the parent
+
+                    textForm.Text = openFileDialog.SafeFileName;    
+
+                    //Show the form
+                    textForm.Show();
+                }
+                catch
+                {
+                    MessageBox.Show("Could not open selected file");
+                }
+            }
+        }
+
+        private void toolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
